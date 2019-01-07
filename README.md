f374bc6cb9cc40ceb23dc2cd639f63f2fd67ca81
Vanessa Naja <vcpn@yahoo.com>
2019-01-07 23:51:57 +0000

    Set up app.js and add README

---
+++ README.md
@@ -0,0 +1,4 @@
+This repository is for the RESTful blog project in Colt Steele's course "The Web Developer Bootcamp".
+
+The purpose is to practice all we've learned so far and add what we're currently learning
+about RESTful Routing.
--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express    = require("express"),
       app        = express(),
-      bodyParser = require("bodyParser"),
-      mongodb    = require("mongoose");
+      bodyParser = require("body-parser"),
+      mongoose    = require("mongoose");
+      
+mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// title
+// image
+// body
+// created
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is running!")
+})
