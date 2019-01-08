f65c6b4b4a6a1d4a25c029237934455f4c9c219f
Vanessa Naja <vcpn@yahoo.com>
2019-01-08 21:33:40 +0000

    Add post route

--- app.js
+++ app.js
@@ -35,6 +35,21 @@ app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
+app.get("/blogs/new", function(req, res){
+   res.render("new"); 
+});
+
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+       if(err){
+           alert("something went wrong, please try again");
+           res.render("new");
+       } else {
+           res.redirect("/blogs");
+       }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running!")
 })
