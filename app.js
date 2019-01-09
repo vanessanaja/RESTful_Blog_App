83b3dd1a7a7ec3e69c7d578635e05ac401bebc28
Vanessa Naja <vcpn@yahoo.com>
2019-01-09 18:33:43 +0000

    add edit route

--- app.js
+++ app.js
@@ -77,6 +77,17 @@ app.get("/blogs/:id", function(req, res){
    });
 });
 
+// EDIT Route
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog}); 
+        }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running!")
 })
